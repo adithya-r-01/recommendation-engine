fc0fb1469ab7dd70438c23857b0c192a5e239af2
Adithya <aramanujam7@gatech.edu>
2021-02-03 21:15:56 -0500

    Added the Popularity Functionality
    Run via CLI by java  Driver [threshold, how_many]

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,7 +8,9 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import java.io.File;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
@@ -22,28 +24,128 @@ public class Driver {
 
         public static void main(String[] args) {
 
-            float threshold = Float.parseFloat(args[0]);
-            int usrId = Integer.parseInt(args[1]);
-            int howMany = Integer.parseInt(args[2]);
+            float threshold = (float) 0.0;
+            int usrId = 0;
+            int howMany = 0;
+            boolean user = true;
+
+            try {
+                if (args.length == 3) {
+                    user = true;
+                    threshold = Float.parseFloat(args[0]);
+                    usrId = Integer.parseInt(args[1]);
+                    howMany = Integer.parseInt(args[2]);
+                }
+                if (args.length == 2) {
+                    user = false;
+                    threshold = Float.parseFloat(args[0]);
+                    howMany = Integer.parseInt(args[1]);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Invalid command line argument!");
+            }
 
             BasicConfigurator.configure();
 
-            try {
-                DataModel model = new FileDataModel(new File("dataset.csv"));
-                CityBlockSimilarity similarity = new CityBlockSimilarity(model);
-                UserNeighborhood neighborhood = new ThresholdUserNeighborhood(threshold,similarity, model);
-                UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+            if (user) {
+                try {
+                    List<RecommendedItem> recommendations = userRecc(threshold, usrId, howMany);
+                    for (RecommendedItem recommend: recommendations) {
+                        System.out.println(recommend);
 
-                // The First argument is the userID and the Second parameter is 'HOW MANY'
-                List<RecommendedItem> recommendations = recommender.recommend(usrId, howMany);
-
-                for (RecommendedItem recommendation : recommendations) {
-                    System.out.println(recommendation);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                System.out.println("Exception occurred!");
+
+            } else {
+                try {
+                    TreeMap<Long, Float> populars = popularity(threshold, howMany);
+                    int count = 0;
+                    //LinkedHashMap preserve the ordering of elements in which they are inserted
+                    LinkedHashMap<Long, Float> reverseSortedMap = new LinkedHashMap<>();
+
+                    populars.entrySet()
+                            .stream()
+                            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                            .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+
+                    System.out.println(reverseSortedMap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
         }
+
+        private static List<RecommendedItem> userRecc (float threshold, int usrId, int howMany) throws Exception {
+            List<RecommendedItem> recommendations;
+
+            DataModel model = new FileDataModel(new File("dataset.csv"));
+            CityBlockSimilarity similarity = new CityBlockSimilarity(model);
+            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(threshold,similarity, model);
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+            // The First argument is the userID and the Second parameter is 'HOW MANY'
+            recommendations = recommender.recommend(usrId, howMany);
+
+
+            return recommendations;
+        }
+
+        private static TreeMap<Long, Float> popularity(float threshold, int howMany) throws Exception {
+            TreeMap<Long, Float> tM = new TreeMap<>();
+            Set<Integer> users = getUsers();
+
+            for (Integer user: users) {
+                List<RecommendedItem> temp = userRecc(threshold, user, howMany);
+                for (RecommendedItem recommend: temp) {
+                    if (tM.containsKey(recommend.getItemID())) {
+                        tM.replace(recommend.getItemID(), tM.get(recommend.getItemID()) + recommend.getValue());
+                    } else {
+                        tM.put(recommend.getItemID(), recommend.getValue());
+                    }
+                }
+            }
+
+            return tM;
+        }
+
+        private static Set<Integer> getUsers() {
+
+            Set<Integer> users = new HashSet<>();
+
+            String fileName= "dataset.csv";
+            File file= new File(fileName);
+
+            List<List<String>> lines = new ArrayList<>();
+            Scanner inputStream;
+
+            try {
+                inputStream = new Scanner(file);
+
+                while(inputStream.hasNext()){
+                    String line= inputStream.next();
+                    String[] values = line.split(",");
+                    lines.add(Arrays.asList(values));
+                }
+
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            for(List<String> line: lines) {
+                for (String value: line) {
+                    users.add(Integer.parseInt(value));
+                    break;
+                }
+            }
+
+            return users;
+        }
+
 
 }
